dffdd505c1bbdea3e95600c84817b91708e49e29
Je-0204 <dlej0204@naver.com>
2026-01-03 20:10:01 +0900

    [feature/15] Remove Unused Import

--- app/src/main/java/com/yourun_compose/ui/components/YourunTextField.kt
+++ app/src/main/java/com/yourun_compose/ui/components/YourunTextField.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
