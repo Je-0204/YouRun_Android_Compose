325cbe15544572cafe8b0588e76d83fcff17ff61
Je-0204 <dlej0204@naver.com>
2026-01-05 18:03:15 +0900

    [feature/17] Remove Unused Import

--- app/src/main/java/com/yourun_compose/MainActivity.kt
+++ app/src/main/java/com/yourun_compose/MainActivity.kt
@@ -4,18 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.yourun_compose.ui.screen.LoginScreen
-import com.yourun_compose.ui.screen.SplashScreen
 import com.yourun_compose.ui.theme.YouRun_composeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
